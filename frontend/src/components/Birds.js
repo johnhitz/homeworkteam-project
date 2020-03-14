5b4536575e547b954dbbd62ac15cefb4738a3cf6
Ric Mershon <rmersh@ricmershon.com>
2020-03-14 17:50:14 -0400

    Added delete and new react functions.

--- frontend/src/components/Birds.js
+++ frontend/src/components/Birds.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ShowBird from './ShowBird'
+import NewBird from './NewBird'
 
 let baseURL = 'http://localhost:3003/birds'
 export default class Birds extends Component {
@@ -14,6 +15,8 @@ export default class Birds extends Component {
     }
     this.getBirds = this.getData.bind(this)
     this.getBird = this.getBird.bind(this)
+    this.handleAddBird = this.handleAddBird.bind(this)
+    this.deleteBird = this.deleteBird.bind(this)
   }
   componentDidMount(){
     this.getData()
@@ -34,19 +37,45 @@ export default class Birds extends Component {
     this.setState({bird: bird})
   }
 
+  handleAddBird(bird) {
+      const copyBirds = [bird, ...this.state.birds]
+      this.setState({
+          birds: copyBirds
+      })
+  }
+
+  async deleteBird(id) {
+      try {
+          let response = await fetch(`${baseURL}/${id}`, {
+              method: 'DELETE'
+          })
+          let data = await response.json()
+          const foundBird = this.state.birds.findIndex(bird => bird._id === id)
+          const copyBirds = [...this.state.birds]
+          copyBirds.splice(foundBird, 1)
+          this.setState({birds: copyBirds})
+      } catch (error) {
+          console.error(error);
+      }
+  }
 
   render() {
     console.log(`this is it`, this.state.bird)
     return(
       <div className="container">
         <h4 className="">Birds</h4>
+        <NewBird handleAddBird={this.handleAddBird} baseURL={baseURL}/>
         {
             this.state.birds.map(bird => {
                 return(
                     <div key={bird._id}>
                     <h5 onClick={() => {
-                      this.getBird(bird)
-                    }}>{bird.name} <span>X</span></h5>
+                      this.getBird(bird) }}>
+                        {bird.name}
+                        <span onClick={() => { this.deleteBird(bird._id)}}>
+                            X
+                        </span>
+                    </h5>
                     </div>
                 )
 
